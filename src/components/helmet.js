0b2fee3bdc334a706c96e2b3d6666bc98718e34a
ceva24 <2307084+ceva24@users.noreply.github.com>
2019-09-13 22:52:29 +0100

    moved helmet to a separate component

---
+++ src/components/helmet.js
@@ -0,0 +1,8 @@
+import Helmet from "react-helmet";
+import React from "react";
+
+export default () => (
+    <Helmet>
+        <title>Chris Evans | Thoughts, code and everything in-between</title>
+    </Helmet>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,11 @@
 import React from "react"
-import Helmet from "react-helmet"
+import Helmet from "./helmet"
 import Header from "./header"
 import Menu from "./menu"
 
 export default ({ children }) => (
-    <div style={{ backgroundColor: 'white', margin: '0 auto', minHeight: 1100, maxWidth: 800, padding: '3rem' }}>
-        <Helmet>
-            <title>Chris Evans | Thoughts, code and everything in-between</title>
-        </Helmet>
+    <div style={{ margin: '0 auto', minHeight: 1100, maxWidth: 800, padding: '3rem' }}>
+        <Helmet />
         <Header />
         <Menu />
         {children}

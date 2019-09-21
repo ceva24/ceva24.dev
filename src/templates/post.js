7d0f57bd377390d593a9ce06fc672ec872032056
ceva24 <2307084+ceva24@users.noreply.github.com>
2019-09-22 00:24:00 +0100

    add a link back to the home page at the end of each post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "src/components/layout"
 
 export default ({ data }) => (
     <Layout>
         <h2>{data.markdownRemark.frontmatter.title}</h2>
         <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        <Link to="/">Home</Link>
     </Layout>
 )
 

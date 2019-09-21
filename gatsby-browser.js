64df487f80888e07c3dbc472599b2750a60f1368
ceva24 <2307084+ceva24@users.noreply.github.com>
2019-09-21 11:49:56 +0100

    load fontawesome css to prevent style changes during page load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,6 @@
 import "typeface-source-sans-pro"
 import "typeface-merriweather"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+
+config.autoAddCss = false

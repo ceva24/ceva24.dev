e0e87d3466a4cdb0e8fa67a6c291ceea6fbb1d3f
ceva24 <2307084+ceva24@users.noreply.github.com>
2019-09-22 19:09:14 +0100

    add date to post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,22 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "src/utils/typography"
 import Layout from "src/components/layout"
 
 export default ({ data }) => (
     <Layout>
-        <h2>{data.markdownRemark.frontmatter.title}</h2>
+        <h2 css={css`
+            margin-bottom: 0;
+        `}>
+            {data.markdownRemark.frontmatter.title}
+        </h2>
+        <div css={css`
+            color: #7f8c8d;
+            margin-bottom: ${rhythm(3 / 4)};
+        `}>
+            <strong>{data.markdownRemark.frontmatter.date}</strong>
+        </div>
         <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
         <Link to="/">Home</Link>
     </Layout>
@@ -16,6 +28,7 @@ export const query = graphql`
       html
       frontmatter {
         title
+        date
       }
     }
   }

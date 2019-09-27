c7d76b466a7c1d735533ae75f7b41021f84f9230
ceva24 <2307084+ceva24@users.noreply.github.com>
2019-09-27 21:00:03 +0100

    update styles to improve mobile usability

--- src/components/layout/bio.js
+++ src/components/layout/bio.js
@@ -15,11 +15,13 @@ export default (props) => (
     `}>
         <img
             src={ProfilePicture}
-            alt="Chris Evans' profile picture"
+            alt="Chris Evans head shot"
             css={css`
                 margin-right: ${rhythm(1 / 2)};
                 margin-bottom: 0;
                 border-radius: 100%;
+                min-height: ${rhythm(3)};
+                min-width: ${rhythm(3)};
                 height: ${rhythm(3)};
                 width: ${rhythm(3)};
                 box-shadow: none;
@@ -28,23 +30,24 @@ export default (props) => (
             padding-left: ${rhythm(3 / 4)};
             border-left: 1px solid;
         `}>
-            <div css={css`
-                font-size: ${rhythm(4 / 5)};
-            `}>
-                <h1>
-                    <Link to="/" css={css`
+            <h1>
+                <Link to="/" css={css`
+                    color: inherit;
+                    &:hover {
                         color: inherit;
-                        &:hover {
-                            color: inherit;
-                        }
-                    `}>
-                        {props.name}
-                    </Link>
-                </h1>
-            </div>
+                    }
+                `}>
+                    {props.name}
+                </Link>
+            </h1>
             <div>A Web Development Team Leader at the University of York</div>
             <div css={css`
-                font-size: ${rhythm(4 / 5)};
+                @media(max-width: 768px) {
+                    font-size: ${rhythm(1)};
+                }
+                @media(min-width: 768px) {
+                    font-size: ${rhythm(4 / 5)};
+                }
             `}>
                 <SocialIcon icon={faEnvelope} label="Email" url="mailto:chris@ceva24.dev" />
                 <SocialIcon icon={faGithub} label="Github" url="https://www.github.com/ceva24" />
